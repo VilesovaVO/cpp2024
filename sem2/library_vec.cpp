471858b919462e7e1fdc69ec6882002da31c75d7
VilesovaVO <Lerochka03012414@gmail.com>
2025-03-11 21:23:25 +0300

    MiniHW1.2

---
+++ sem2/library_vec.cpp
@@ -0,0 +1,48 @@
+﻿#include <vector>
+#include <algorithm>
+#include <cmath>
+
+
+float vec_average(std::vector<float> M) {
+    float sm = 0;
+
+    for (int i = 0; i < M.size(); i++) {
+        sm += M[i];
+    }
+
+    float avg = sm / M.size();
+    return avg;
+}
+
+float vec_mediana(std::vector<float> M) {
+    float mediana = 0;
+    sort(M.begin(), M.end());
+
+    if (M.size() % 2 == 0) {
+        mediana = (M[M.size() / 2] + M[M.size() / 2 - 1]) / 2;
+    }
+    else mediana = M[M.size() / 2];
+
+    return mediana;
+}
+
+float vec_asd(std::vector<float> M) {
+    float sm = 0;
+
+    for (int i = 0; i < M.size(); i++)
+        sm += M[i] * M[i];
+
+    float sd = sqrt(sm / M.size());
+    return sd;
+}
+
+float vec_dispersia(std::vector<float> M) {
+    float avg = vec_average(M);
+    float sm = 0;
+
+    for (int i = 0; i < M.size(); i++)
+        sm += (avg - M[i]) * (avg - M[i]);
+
+    float d = sqrt(sm / M.size());
+    return d;
+}
---
+++ sem2/vector.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <vector>
+#include "library_vec.cpp"
+int main() {
+    std::vector<float> M = { 2, 4, 6, 12, 5 };
+    std::cout << vec_dispersia(M) << std::endl;
+    std::cout << vec_average(M) << std::endl;
+    std::cout << vec_mediana(M) << std::endl;
+    std::cout << vec_asd(M) << std::endl;
+}
