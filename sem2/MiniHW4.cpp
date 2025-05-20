ad5096e4394a9047dbeb7d53b90ce47f38e6cf28
VilesovaVO <Lerochka03012414@gmail.com>
2025-05-20 20:17:30 +0300

    Create MiniHW4.cpp

---
+++ sem2/MiniHW4.cpp
@@ -0,0 +1,14 @@
+﻿#include <vector>
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+int main() {
+    std::vector<std::string> strings = { "Cat", "game", "hamburgers", "lambda" };
+
+    auto count_chars = [](const std::string& str) { return str.size(); };
+
+    for (const auto& str : strings) {
+        std::cout << count_chars(str) << "\n";
+    }
+}
